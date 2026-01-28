016c48a39366e287f3997c0826f026ff041af60e
Cameron Wilson <144940072+dontgtg123@users.noreply.github.com>
2026-01-28 12:21:57 -0500

    started work on objects and world

--- RaytracerR/src/main.rs
+++ RaytracerR/src/main.rs
@@ -2,13 +2,76 @@
 extern crate image;
 extern crate nalgebra as na;
 
+use std::collections::hash_set::Intersection;
 use image::{ImageBuffer, RgbImage, Rgb};
+use na::{Transform3, Point3, Vector3};
+
+pub struct Ray {
+    pub origin: Point3<f32>,
+    pub direction: Vector3<f32>
+}
 
 pub struct Camera {
-    position: na::Point3<f32>,
-    lookat: na::Vector3<f32>,
-    up: na::Vector3<f32>
-} 
+    position: Point3<f32>,
+    lookat: Vector3<f32>,
+    up: Vector3<f32>
+}
+
+pub struct Object {
+    color: Vector3<f32>,
+    position: Point3<f32>,
+}
+
+pub trait Intersects {
+    fn intersect(&self, ray: &Ray) -> Option<Object>;
+}
+
+pub struct Sphere {
+    pub center: Point3<f32>,
+    pub radius: f32
+}
+
+impl Intersects for Sphere {
+    fn intersect(&self, ray: &Ray) -> Option<Object> {
+        return Option::None;
+    }
+}
+
+pub struct Triangle {
+    pub vertices: Vec<Point3<f32>>,
+    pub normal: Vector3<f32>,
+}
+
+impl Intersects for Triangle {
+    fn intersect(&self, ray: &Ray) -> Option<Object> {
+        return Option::None;
+    }
+}
+
+struct World {
+    objects: Vec<Object>,
+    //TODO attributes
+}
+
+impl World {
+    pub fn new() -> World {
+        return World { objects: vec![] };
+    }
+
+    pub fn add(&mut self, object: Object) {
+        self.objects.push(object);
+    }
+
+    pub fn transform(object: &mut Object, transform3: Transform3<f32>) {
+        object.position = transform3 * object.position;
+    }
+
+    pub fn transform_all_objects(&mut self, transform3: Transform3<f32>) {
+        for object in self.objects.iter_mut() {
+            Self::transform(object, transform3);
+        }
+    }
+}
 
 fn main() {
     const WIDTH: u32 = 100;
