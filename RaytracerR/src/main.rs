991e9841adf5a87c3b160e15d7cf3ae79b42442a
caw47 <caw47@rit.edu>
2026-01-27 22:38:10 -0500

    start

--- RaytracerR/src/main.rs
+++ RaytracerR/src/main.rs
@@ -1,3 +1,22 @@
+
+extern crate image;
+extern crate nalgebra as na;
+
+use image::{ImageBuffer, RgbImage, Rgb};
+
+pub struct Camera {
+    position: na::Point3<f32>,
+    lookat: na::Vector3<f32>,
+    up: na::Vector3<f32>
+} 
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: u32 = 100;
+    const HEIGHT: u32 = 100;
+
+    let mut img: RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
+
+    img.put_pixel(50, 50, Rgb([255, 0, 0]));
+
+    img.save("output/test.png").unwrap();
 }
