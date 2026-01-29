4b5d12bb635cd7693842e280cffa6efa95423ca7
Cameron Wilson <cameronwil.be@gmail.com>
2026-01-29 15:06:25 -0500

    quick commit, wip

--- RaytracerR/src/main.rs
+++ RaytracerR/src/main.rs
@@ -4,11 +4,21 @@ extern crate nalgebra as na;
 
 use std::collections::hash_set::Intersection;
 use image::{ImageBuffer, RgbImage, Rgb};
-use na::{Transform3, Point3, UnitVector3, Vector3};
+use na::{Transform3, Point3, UnitVector3, Vector3, Scale3, Scale, Similarity3};
 
 pub struct Ray {
     pub origin: Point3<f32>,
     pub direction: UnitVector3<f32>
+}
+
+pub struct HitRecord {
+    pub point: Point3<f32>,
+    pub normal: UnitVector3<f32>,
+}
+
+pub enum Shape {
+    Sphere(Sphere),
+    Triangle(Triangle),
 }
 
 impl Ray {
@@ -24,34 +34,69 @@ pub struct Camera {
 }
 
 pub struct Object {
-    color: UnitVector3<f32>,
-    position: Point3<f32>,
+    //TODO material
+    color: Vector3<f32>,
+    shape: Shape,
+    transform: Similarity3<f32>,
 }
 
-pub trait Intersects {
-    fn intersect(&self, ray: &Ray) -> Option<Object>;
+impl Object {
+    pub fn new(color: Vector3<f32>, shape: Shape, transform: Similarity3<f32>) -> Object {
+        return Object {color, shape, transform };
+    }
+}
+
+trait Intersects {
+    fn intersect(&self, ray: &Ray) -> Option<HitRecord>;
+    //fn transform(&mut self, transform: &Similarity3<f32>);
+    //fn apply_mvp(&mut self, model_matrix: Matrix4<f32>, view_matrix: Matrix4<f32>, projection_matrix: Matrix4<f32>);
 }
 
 pub struct Sphere {
-    pub center: Point3<f32>,
-    pub radius: f32
+    center: Point3<f32>,
+    radius: f32,
+}
+
+impl Sphere {
+    pub fn new(center: Point3<f32>, radius: f32) -> Self {
+        return Sphere { center, radius };
+    }
 }
 
 impl Intersects for Sphere {
-    fn intersect(&self, ray: &Ray) -> Option<Object> {
+    fn intersect(&self, ray: &Ray) -> Option<HitRecord> {
         return Option::None;
     }
+
+    // fn transform(&mut self, transform: &Similarity3<f32>) {
+    //     self.center = transform.isometry.transform_point(&self.center);
+    //
+    //     self.radius = self.radius * transform.scaling();
+    // }
+
+    //fn apply_mvp(&mut self, model_matrix: Matrix4<f32>, view_matrix: Matrix4<f32>, projection_matrix: Matrix4<f32>) {
+    //    let mvp = projection_matrix * view_matrix * model_matrix;
+
+    //    self.center = mvp.transform_point(&self.center);
+
+    //}
 }
 
 pub struct Triangle {
-    pub vertices: Vec<Point3<f32>>,
-    pub normal: UnitVector3<f32>,
+    vertices: Vec<Point3<f32>>,
+    normal: UnitVector3<f32>,
 }
 
 impl Intersects for Triangle {
-    fn intersect(&self, ray: &Ray) -> Option<Object> {
+    fn intersect(&self, ray: &Ray) -> Option<HitRecord> {
         return Option::None;
     }
+
+    // fn transform(&mut self, transform: &Similarity3<f32>) {
+    //     for vertex in self.vertices.iter_mut() {
+    //         *vertex = transform.transform_point(vertex);
+    //     }
+    // }
 }
 
 struct World {
@@ -68,24 +113,40 @@ impl World {
         self.objects.push(object);
     }
 
-    pub fn transform(object: &mut Object, transform3: Transform3<f32>) {
-        object.position = transform3 * object.position;
+    pub fn transform(&self, object: &mut Object) {
+        match &mut object.shape {
+            Shape::Sphere(sphere) => {
+                sphere.radius = sphere.radius * object.transform.scaling();
+                sphere.center = object.transform.isometry.translation.transform_point(&sphere.center);
+            },
+            Shape::Triangle(triangle) => {
+                triangle.vertices = triangle.vertices.iter()
+                    .map(|v| object.transform.transform_point(v)).collect();
+            }
+        }
     }
-
-    pub fn transform_all_objects(&mut self, transform3: Transform3<f32>) {
-        for object in self.objects.iter_mut() {
-            Self::transform(object, transform3);
+    pub fn transform_all_objects(&mut self, transform: &Similarity3<f32>) {
+        for object in &mut self.objects {
+            object.transform(transform);
         }
     }
 }
 
 fn main() {
-    const WIDTH: u32 = 100;
-    const HEIGHT: u32 = 100;
+    let mut world = World::new();
 
-    let mut img: RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
+    let sphere1 = Sphere::new(Point3::new(0.0, 0.0, 0.0), 1.0);
 
-    img.put_pixel(50, 50, Rgb([255, 0, 0]));
+    sphere1.transform(&Similarity3::identity());
 
-    img.save("output/test.png").unwrap();
+    world.add(Box::new(sphere1));
+    &mut world.transform(&Transform3::identity());
+    //const WIDTH: u32 = 100;
+    //const HEIGHT: u32 = 100;
+
+    //let mut img: RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
+
+    //img.put_pixel(50, 50, Rgb([255, 0, 0]));
+
+    //img.save("output/test.png").unwrap();
 }
